765295b875a726abe57c51d9cfae98c144b39234
Max Keller <max@klauskeller.com>
2020-08-12 14:05:14 +0200

    created models Restaurant Review and seeded db with 5 Restaurants

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Creating some restaurants'
+Restaurant.destroy_all
+
+5.times do
+  r = Restaurant.create!({ address: Faker::Address.city,
+                           name: Faker::FunnyName.two_word_name,
+                           phone_number: Faker::PhoneNumber.phone_number,
+                           category: ["chinese", "italian", "japanese", "french", "belgian"].sample })
+  puts "Created restaurant with ID: #{r.id}"
+end
+
+puts "Created #{Restaurant.count} new restaurants."
